c1222e24057945cd859bb207c92cb292c37ec8df
404pnf <404pnf@exmaple.org>
2013-04-24 13:48:19 +0800

    mac osx 的dd不识别G只识别m，因此我们将G换算为m后再传给dd

--- sh-script/generate-files.rb
+++ sh-script/generate-files.rb
@@ -13,9 +13,11 @@ fib = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597]
 # call bash command
 # system( touch 'touch.txt' )
 
+# mac osx 不认 G 这个，但可以人m，因此我们把size乘以1024
 # dd 不加任何 of 的话默认输出是当前目录
 arr = ('a'..'z').to_a | (0..9).to_a
 (prime + fib).each { |size|
+  mega = size * 1024
   filename = 'ziyuan-jiami-' + arr.shuffle.take(6).join + '.crypt'
-  system( "dd if=/dev/zero of=#{filename} bs=1 count=0 seek=#{size}G" )
+  system( "dd if=/dev/zero of=#{filename} bs=1 count=0 seek=#{mega}m" )
 }
